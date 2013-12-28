c18719454c0d1144460fb195f4d3f9b3f7943965
Simon Robb <simon@simonrobb.com.au>
2013-12-28 17:28:15 +1100

    Added basic download page & styling

---
+++ download/compile.php
@@ -0,0 +1,4 @@
+<?php
+
+var_dump (file_exists('index.php'));
+exit ();
---
+++ download/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	
+	<!-- Scripts -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	
+	<!-- Style sheets -->
+	<link href="/css/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+	<link href="/css/print.css" media="print" rel="stylesheet" type="text/css" />
+	<!--[if IE]>
+	    <link href="/css/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+	<![endif]-->
+	
+	<!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	
+	<div class="wrapper">
+		
+		<h1>Dialup</h1>
+		<span id="headingTag">Data widgets for jQuery</span>
+		
+		<a id="downloadButton" class="btn has-image">
+			
+			<span class="icons-download"></span>
+			<span class="line-one">Download full package</span>
+			<span class="line-two">32kB (minified)</span>
+		</a>
+		
+		<a href="https://github.com/simonrobb/dialup"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
+	</div>	
+</body>
+</html>
